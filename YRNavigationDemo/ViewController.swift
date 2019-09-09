ab21293fe8458f9bf8e9a82c7ecf7a93ef554fa1
yangrui <1075551032@qq.com>
2019-09-09 14:58:54 +0800

    Initial Commit

---
+++ YRNavigationDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YRNavigationDemo
+//
+//  Created by kilrae on 2019/9/9.
+//  Copyright © 2019 kilrae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
